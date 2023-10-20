cd1d42cd49bbc595a6ff068680ce65886a3f371d
aninha998765 <130669141+aninha998765@users.noreply.github.com>
2023-10-20 10:07:57 -0300

    commit zinatto

---
+++ metodo_pessoas.php
@@ -0,0 +1,545 @@
+<?php
+
+include("Classe_do_metodo_pessoas.php");
+
+$p1 = new Pessoa();
+
+$p1->nome = "Ana Zinatto";
+$p1->altura = 1.60;
+$p1->nascimento = "03-11-2006";
+$p1->hobbie = "Treinar e ouvir música";
+
+echo "Nome:" .$p1->nome. "<br/>
+    Altura: ".$p1->altura."<br/>
+    Nascimento: ".$p1->nascimento."<br/>
+    hobbie: ".$p1->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+    $p2 = new Pessoa();
+
+    $p2->nome = "Pedro Santos";
+    $p2->altura = 1.80;
+    $p2->nascimento = "24-10-2006";
+    $p2->hobbie = "Tocar violão";
+    
+    echo "Nome:" .$p2->nome. "<br/>
+    Altura: ".$p2->altura. "<br/>
+    Nascimento: ".$p2->nascimento. "<br/>
+    Hobbie: ".$p2->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p3 = new Pessoa();
+
+    $p3->nome = "Guilherme Carvalho";
+    $p3->altura = 1.75;
+    $p3->nascimento = "09-06-2007";
+    $p3->hobbie = "Jogar Videogame";
+    
+    echo "Nome:" .$p3->nome. "<br/>
+    Altura: ".$p3->altura. "<br/>
+    Nascimento: ".$p3->nascimento. "<br/>
+    Hobbie: ".$p3->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p4 = new Pessoa();
+
+    $p4->nome = "Júlia Mogi";
+    $p4->altura = 1.70;
+    $p4->nascimento = "08-11-2006";
+    $p4->hobbie = "Cozinhar";
+    
+    echo "Nome:" .$p4->nome. "<br/>
+    Altura: ".$p4->altura. "<br/>
+    Nascimento: ".$p4->nascimento. "<br/>
+    hobbie: ".$p4->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+
+    $p5 = new Pessoa();
+
+    $p5->nome = "Laura Perez";
+    $p5->altura = 1.70;
+    $p5->nascimento = "03-08-2006";
+    $p5->hobbie = "Ler";
+    
+    echo "Nome:" .$p5->nome. "<br/>
+    Altura: ".$p5->altura. "<br/>
+    Nascimento: ".$p5->nascimento. "<br/>
+    hobbie: ".$p5->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p6 = new Pessoa();
+
+    $p6->nome = "Natally Reis";
+    $p6->altura = 1.60;
+    $p6->nascimento = "07-01-2006";
+    $p6->hobbie = "Ouvir música";
+    
+    echo "Nome:" .$p6->nome. "<br/>
+    Altura: ".$p6->altura. "<br/>
+    Nascimento: ".$p6->nascimento. "<br/>
+    hobbie: ".$p6->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    $p7 = new Pessoa();
+
+    $p7->nome = "Samauel Américo";
+    $p7->altura = 1.85;
+    $p7->nascimento = "26-01-2007";
+    $p7->hobbie = "Tocar Piano";
+    
+    echo "Nome:" .$p7->nome. "<br/>
+    Altura: ".$p7->altura. "<br/>
+    Nascimento: ".$p7->nascimento. "<br/>
+    hobbie: ".$p7->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+    $p8 = new Pessoa();
+
+    $p8->nome = "Beatriz Baccaro";
+    $p8->altura = 1.60;
+    $p8->nascimento = "01-08-2006";
+    $p8->hobbie = "Esporte";
+    
+    echo "Nome:" .$p8->nome. "<br/>
+    Altura: ".$p8->altura. "<br/>
+    Nascimento: ".$p8->nascimento. "<br/>
+    hobbie: ".$p8->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    
+    $p9 = new Pessoa();
+
+    $p9->nome = "Pedro Grechi";
+    $p9->altura = 1.69;
+    $p9->nascimento = "23-03-2007";
+    $p9->hobbie = "Jogar e desenhar";
+    
+    echo "Nome:" .$p9->nome. "<br/>
+    Altura: ".$p9->altura. "<br/>
+    Nascimento: ".$p9->nascimento. "<br/>
+    hobbie: ".$p9->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+    
+    $p10= new Pessoa();
+
+    $p10->nome = "Luiz Gustavo Magela";
+    $p10->altura = 1.77;
+    $p10->nascimento = "06-03-2007";
+    $p10->hobbie = "Dormir e jogar Videogame";
+    
+    echo "Nome:" .$p10->nome. "<br/>
+    Altura: ".$p10->altura. "<br/>
+    Nascimento: ".$p10->nascimento. "<br/>
+    hobbie: ".$p10->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    $p11= new Pessoa();
+
+    $p11->nome = "José Luiz ";
+    $p11->altura = 1.71;
+    $p11->nascimento = "15-09-2006";
+    $p11->hobbie = "Assistir critica de filmes";
+    
+    echo "Nome:" .$p11->nome. "<br/>
+    Altura: ".$p11->altura. "<br/>
+    Nascimento: ".$p11->nascimento. "<br/>
+    hobbie: ".$p11->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    $p11= new Pessoa();
+
+    $p11->nome = "Thell Zardi ";
+    $p11->altura = 1.71;
+    $p11->nascimento = "31-10-2006";
+    $p11->hobbie = "Jogar";
+    
+    echo "Nome:" .$p11->nome. "<br/>
+    Altura: ".$p11->altura. "<br/>
+    Nascimento: ".$p11->nascimento. "<br/>
+    hobbie: ".$p11->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+    $p12= new Pessoa();
+
+    $p12->nome = "Ryan Silveira ";
+    $p12->altura = 1.72;
+    $p12->nascimento = "02-09-2006";
+    $p12->hobbie = "Jogar Videogame";
+    
+    echo "Nome:" .$p12->nome. "<br/>
+    Altura: ".$p12->altura. "<br/>
+    Nascimento: ".$p12->nascimento. "<br/>
+    hobbie: ".$p12->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+    $p13= new Pessoa();
+
+    $p13->nome = "Leticia Gabrielle ";
+    $p13->altura = 1.65;
+    $p13->nascimento = "30-09-2006";
+    $p13->hobbie = "Ouvir música e assistir série";
+    
+    echo "Nome:" .$p13->nome. "<br/>
+    Altura: ".$p13->altura. "<br/>
+    Nascimento: ".$p13->nascimento. "<br/>
+    hobbie: ".$p13->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    $p14= new Pessoa();
+
+    $p14->nome = "Camily Vitória";
+    $p14->altura = 1.60;
+    $p14->nascimento = "22-01-2007";
+    $p14->hobbie = "Ouvir música";
+    
+    echo "Nome:" .$p14->nome. "<br/>
+    Altura: ".$p14->altura. "<br/>
+    Nascimento: ".$p14->nascimento. "<br/>
+    hobbie: ".$p14->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+    $p15= new Pessoa();
+
+    $p15->nome = "Ana Clara Maciel";
+    $p15->altura = 1.50;
+    $p15->nascimento = "19-01-2007";
+    $p15->hobbie = "Ler e ouvir música";
+    
+    echo "Nome:" .$p15->nome. "<br/>
+    Altura: ".$p15->altura. "<br/>
+    Nascimento: ".$p15->nascimento. "<br/>
+    hobbie: ".$p15->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p16= new Pessoa();
+
+    $p16->nome = "Gustavo Otaviano ";
+    $p16->altura = 1.85;
+    $p16->nascimento = "04-03-2006";
+    $p16->hobbie = "Esportes";
+    
+    echo "Nome:" .$p16->nome. "<br/>
+    Altura: ".$p16->altura. "<br/>
+    Nascimento: ".$p16->nascimento. "<br/>
+    hobbie: ".$p16->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    $p17= new Pessoa();
+
+    $p17->nome = "Henrique Tavares ";
+    $p17->altura = 1.63;
+    $p17->nascimento = "26-07-2006";
+    $p17->hobbie = "Jogar e ouvir música";
+    
+    echo "Nome:" .$p17->nome. "<br/>
+    Altura: ".$p17->altura. "<br/>
+    Nascimento: ".$p17->nascimento. "<br/>
+    hobbie: ".$p17->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    $p18= new Pessoa();
+
+    $p18->nome = "Gabriel Credendio ";
+    $p18->altura = 1.70;
+    $p18->nascimento = "28-07-2006";
+    $p18->hobbie = "Jogar Videogame";
+    
+    echo "Nome:" .$p18->nome. "<br/>
+    Altura: ".$p18->altura. "<br/>
+    Nascimento: ".$p18->nascimento. "<br/>
+    hobbie: ".$p18->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    
+    
+    $p19= new Pessoa();
+
+    $p19->nome = "Raí Franca ";
+    $p19->altura = 1.82;
+    $p19->nascimento = "12-09-2006";
+    $p19->hobbie = "Esportes e ouvir música";
+    
+    echo "Nome:" .$p19->nome. "<br/>
+    Altura: ".$p19->altura. "<br/>
+    Nascimento: ".$p19->nascimento. "<br/>
+    hobbie: ".$p19->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    
+    $p20= new Pessoa();
+
+    $p20->nome = "Ana Clara Ferreira ";
+    $p20->altura = 1.55;
+    $p20->nascimento = "21-03-2007";
+    $p20->hobbie = "Pintar";
+    
+    echo "Nome:" .$p20->nome. "<br/>
+    Altura: ".$p20->altura. "<br/>
+    Nascimento: ".$p20->nascimento. "<br/>
+    hobbie: ".$p20->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+    $p21= new Pessoa();
+
+    $p21->nome = "Gabriel Alves ";
+    $p21->altura = 1.84;
+    $p21->nascimento = "03-08-2006";
+    $p21->hobbie = "Praticar esportes";
+    
+    echo "Nome:" .$p21->nome. "<br/>
+    Altura: ".$p21->altura. "<br/>
+    Nascimento: ".$p21->nascimento. "<br/>
+    hobbie: ".$p21->hobbie. "<br/><br/><br/><br/><br/>";
+    
+
+
+
+ $p22= new Pessoa();
+
+    $p22->nome = "Kauã Lira ";
+    $p22->altura = 1.63;
+    $p22->nascimento = "05-01-2007";
+    $p22->hobbie = "Tocar Guitarra";
+    
+    echo "Nome:" .$p22->nome. "<br/>
+    Altura: ".$p22->altura. "<br/>
+    Nascimento: ".$p22->nascimento. "<br/>
+    hobbie: ".$p22->hobbie. "<br/><br/><br/><br/><br/>";
+    
+
+
+
+
+
+    $p23= new Pessoa();
+
+    $p23->nome = "Gabriel Dias";
+    $p23->altura = 1.74;
+    $p23->nascimento = "21-06-2007";
+    $p23->hobbie = " Jogar";
+    
+    echo "Nome:" .$p23->nome. "<br/>
+    Altura: ".$p23->altura. "<br/>
+    Nascimento: ".$p23->nascimento. "<br/>
+    hobbie: ".$p23->hobbie. "<br/><br/><br/><br/><br/>";
+    
+
+
+
+
+
+    $p24= new Pessoa();
+
+    $p24->nome = " Kauan Silva ";
+    $p24->altura = 1.68;
+    $p24->nascimento = "02-09-2006";
+    $p24->hobbie = " Jogar Videogame";
+    
+    echo "Nome:" .$p24->nome. "<br/>
+    Altura: ".$p24->altura. "<br/>
+    Nascimento: ".$p24->nascimento. "<br/>
+    hobbie: ".$p24->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p25= new Pessoa();
+
+    $p25->nome = "Duda ";
+    $p25->altura = 1.65;
+    $p25->nascimento = "02-04-2007";
+    $p25->hobbie = "Assistir série e jogar volei";
+    
+    echo "Nome:" .$p25->nome. "<br/>
+    Altura: ".$p25->altura. "<br/>
+    Nascimento: ".$p25->nascimento. "<br/>
+    hobbie: ".$p25->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+
+
+
+    $p26= new Pessoa();
+
+    $p26->nome = "Giovana Lupo ";
+    $p26->altura = 1.62;
+    $p26->nascimento = "14-05-2007";
+    $p26->hobbie = "Pintar";
+    
+    echo "Nome:" .$p26->nome. "<br/>
+    Altura: ".$p26->altura. "<br/>
+    Nascimento: ".$p26->nascimento. "<br/>
+    hobbie: ".$p26->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p27= new Pessoa();
+
+    $p27->nome = "Bruno ALves ";
+    $p27->altura = 1.78;
+    $p27->nascimento = "28-05-2007";
+    $p27->hobbie = "Tocar instrumentos";
+    
+    echo "Nome:" .$p27->nome. "<br/>
+    Altura: ".$p27->altura. "<br/>
+    Nascimento: ".$p27->nascimento. "<br/>
+    hobbie: ".$p27->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+
+    $p28= new Pessoa();
+
+    $p28->nome = " Felipe Wilbert ";
+    $p28->altura = 1.78;
+    $p28->nascimento = "06-12-2006";
+    $p28->hobbie = "Academia";
+    
+    echo "Nome:" .$p28->nome. "<br/>
+    Altura: ".$p28->altura. "<br/>
+    Nascimento: ".$p28->nascimento. "<br/>
+    hobbie: ".$p28->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p29= new Pessoa();
+
+    $p29->nome = "Miguel Romero  ";
+    $p29->altura = 1.70;
+    $p29->nascimento = "14-04-2007";
+    $p29->hobbie = "Jogar futebol";
+    
+    echo "Nome:" .$p29->nome. "<br/>
+    Altura: ".$p29->altura. "<br/>
+    Nascimento: ".$p29->nascimento. "<br/>
+    hobbie: ".$p29->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+
+    $p30= new Pessoa();
+
+    $p30->nome = "Henrique Nagata ";
+    $p30->altura = 1.58;
+    $p30->nascimento = "25-10-2006";
+    $p30->hobbie = "Andar de bicicleta";
+    
+    echo "Nome:" .$p30->nome. "<br/>
+    Altura: ".$p30->altura. "<br/>
+    Nascimento: ".$p30->nascimento. "<br/>
+    hobbie: ".$p30->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+    $p31= new Pessoa();
+
+    $p31->nome = "Pedro Araujo ";
+    $p31->altura = 1.68;
+    $p31->nascimento = "24-09-2006";
+    $p31->hobbie = "Andar de bicicleta";
+    
+    echo "Nome:" .$p31->nome. "<br/>
+    Altura: ".$p31->altura. "<br/>
+    Nascimento: ".$p31->nascimento. "<br/>
+    hobbie: ".$p31->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+
+    $p32= new Pessoa();
+
+    $p32->nome = "Beatriz Mayumi ";
+    $p32->altura = 1.65;
+    $p32->nascimento = "03-03-2007";
+    $p32->hobbie = "Ler e jogar";
+    
+    echo "Nome:" .$p32->nome. "<br/>
+    Altura: ".$p32->altura. "<br/>
+    Nascimento: ".$p32->nascimento. "<br/>
+    hobbie: ".$p32->hobbie. "<br/><br/><br/><br/><br/>";
+
+
+
+
+    ?>
+    
+    
+    
+
+
+
